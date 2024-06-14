ed38c96268e06837238567eccd7bd9d5cc3656c7
waclaw_pawel <waclaw.pawel@wp.pl>
2024-06-15 00:28:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,45 @@
 import streamlit as st
-import sqlite3
+import pymysql.cursors
 
-conn = sqlite3.connect('dane.db', check_same_thread=False)
-cursor = conn.cursor()
+# Podanie danych połączenia
+db_host = "sql125.lh.pl"
+db_user = "serwer274744_streamlit"
+db_password = "bazabazA3#"
+db_name = "serwer274744_streamlit"
+
+# Utworzenie połączenia
+db_connection = pymysql.connect(host=db_host,
+                                user=db_user,
+                                password=db_password,
+                                database=db_name,
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+
+cursor = db_connection.cursor()
 
 st.title("Welcome")
 
 def form_creation():
     st.write("Fill the form")
     with st.form(key="Registracion form"):
-        surname = st.text_input('surname: ')
         name = st.text_input('name: ')
-        date = st.date_input('podaj date')
+        age = st.text_input('age: ')
         submit = st.form_submit_button(label='Register')
     
     if submit == True:
         st.success("Success")
-        add_info(surname, name, date)
+        add_info(name, age)
         
     
-def add_info(a,b,c):
+def add_info(name,age):
     cursor.execute(
         """
-        CREATE TABLE IF NOT EXISTS registracion (SURNAME TEXT(50), NAME TEXT(50), DATE TEXT(50) )
+        CREATE TABLE IF NOT EXISTS data (name VARCHAR(50), age VARCHAR(50))
         """
     )
-    cursor.execute("INSERT INTO registracion VALUES (?,?,?)", (a,b,c))
-    conn.commit()
-    conn.close()
+    cursor.execute(f"""INSERT INTO data VALUES (1,2)""")
+    db_connection.commit()
+    db_connection.close()
     st.success("SQLITE update")
 form_creation()
 
